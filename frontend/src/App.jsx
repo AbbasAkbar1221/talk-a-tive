ada0e354ffd845307ff10233fd2a2fa22c6b2d93
AbbasAkbar1221 <abbasakbar765@gmail.com>
2025-02-28 19:34:42 +0530

    initial setup of frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-red-500 text-3xl'>App</h1>
+    </>
+  )
+}
+
+export default App
