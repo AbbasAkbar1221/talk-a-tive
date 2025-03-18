dc162d0a045b6557791252782266e337db008b7f
AbbasAkbar1221 <abbasakbar765@gmail.com>
2025-03-18 23:01:27 +0530

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,13 @@ const Chat = require("./models/chatModel");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
 const io = require("socket.io")(server, {
   cors: {
     origin: "*",
