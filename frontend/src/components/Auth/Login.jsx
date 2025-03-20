539ccb611129435214c254a7f945c7081acff312
AbbasAkbar1221 <abbasakbar765@gmail.com>
2025-03-20 15:02:54 +0530

    add loading state

--- frontend/src/components/Auth/Login.jsx
+++ frontend/src/components/Auth/Login.jsx
@@ -6,6 +6,7 @@ import { ChatState } from "../../context/ChatContext";
 const Login = () => {
   const [formData, setFormData] = useState({ email: "", password: "" });
   const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
   const BACKEND_URL = import.meta.env.VITE_API_URL;
   const { fetchUserDetails } = ChatState();
@@ -17,6 +18,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setError("");
+    setLoading(true);
     try {
       const res = await axios.post(`${BACKEND_URL}/api/auth/login`, formData);
       localStorage.setItem("token", res.data.token);
@@ -24,12 +26,20 @@ const Login = () => {
       navigate("/");
     } catch (err) {
       setError(err.response?.data?.message || "Invalid credentials");
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-md bg-white p-6 rounded-lg shadow-md">
+    <div className="relative flex items-center justify-center min-h-screen bg-gray-100">
+      {loading && (
+        <div className="absolute inset-0 flex items-center justify-center bg-black/30 backdrop-blur-xs  z-50">
+          <div className="w-12 h-12 border-4 border-white border-t-transparent rounded-full animate-spin"></div>
+        </div>
+      )}
+
+      <div className={`w-full max-w-md bg-white p-6 rounded-lg shadow-md ${loading ? "blur-sm" : ""}`}>
         <h2 className="text-2xl font-semibold text-center mb-4">Login</h2>
         {error && <p className="text-red-500 text-sm text-center">{error}</p>}
         <form onSubmit={handleSubmit} className="space-y-4">
@@ -53,9 +63,10 @@ const Login = () => {
           />
           <button
             type="submit"
-            className="w-full bg-blue-600 text-white p-2 rounded hover:bg-blue-700 transition"
+            className="w-full bg-blue-600 text-white p-2 rounded hover:bg-blue-700 transition disabled:opacity-50"
+            disabled={loading}
           >
-            Login
+            {loading ? "Logging in..." : "Login"}
           </button>
         </form>
         <p className="text-sm text-center mt-4">
